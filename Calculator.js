1f8635a78fba50a600fd2db0c5ac8c17b984a52f
Phil Nelson <nelsonpa@miamioh.edu>
2022-05-26 14:38:26 +0900

    completed OOP JS calculator project

--- Calculator.js
+++ Calculator.js
@@ -1,37 +1,95 @@
 export default class Calculator {
     constructor(primaryOperandDisplay, secondaryOperandDisplay, operationDisplay){
-        this.primaryOperandDisplay = primaryOperandDisplay
-        this.secondaryOperandDisplay = secondaryOperandDisplay
-        this.operationDisplay = operationDisplay
+        this.#primaryOperandDisplay = primaryOperandDisplay
+        this.#secondaryOperandDisplay = secondaryOperandDisplay
+        this.#operationDisplay = operationDisplay
 
         this.clear()
     }
 
+    #primaryOperandDisplay
+    #secondaryOperandDisplay
+    #operationDisplay
+
+
     get primaryOperand(){
-        return parseFloat(this.primaryOperandDisplay.dataset.value)
+        return parseFloat(this.#primaryOperandDisplay.dataset.value)
     }
 
     set primaryOperand(value){
-        this.primaryOperandDisplay.dataset.value = value ?? ""
-        this.primaryOperandDisplay.textContent = displayNumber(value)
+        this.#primaryOperandDisplay.dataset.value = value ?? ""
+        this.#primaryOperandDisplay.textContent = displayNumber(value)
+    }
+
+    get secondaryOperand(){
+        return parseFloat(this.#secondaryOperandDisplay.dataset.value)
     }
 
     set secondaryOperand(value){
-        this.secondaryOperandDisplay.dataset.value = value ?? ""
-        this.secondaryOperandDisplay.textContent = displayNumber(value)
+        this.#secondaryOperandDisplay.dataset.value = value ?? ""
+        this.#secondaryOperandDisplay.textContent = displayNumber(value)
+    }
+
+    get operation(){
+        return this.#operationDisplay.textContent
     }
     
     set operation(value){
-        this.operationDisplay.textContent = value ?? ""
+        this.#operationDisplay.textContent = value ?? ""
     }
 
 
     addDigit(digit){
+        if (digit === '.' && this.#primaryOperandDisplay.dataset.value.includes('.')){
+            return
+        }
         if(this.primaryOperand === 0){
             this.primaryOperand = digit
             return
         }
-        this.primaryOperand = this.primaryOperand.toString() + digit
+        this.primaryOperand = this.#primaryOperandDisplay.dataset.value + digit
+    }
+
+    removeDigit(){
+        const numberString = this.#primaryOperandDisplay.dataset.value
+        if (numberString.length <=1){
+            this.primaryOperand = 0
+            return
+        }
+
+        this.primaryOperand = numberString.substring(0, numberString.length - 1)
+    }
+
+    evaluate() {
+        let result
+        switch(this.operation){
+            case '✖️':
+                result = this.secondaryOperand * this.primaryOperand
+                break
+            case '➕':
+                result = this.secondaryOperand + this.primaryOperand
+                break
+            case '➖':
+                result = this.secondaryOperand - this.primaryOperand
+                break
+            case '➗':
+                result = this.secondaryOperand / this.primaryOperand
+                break
+             default:
+                 return   
+        }
+
+        this.clear()
+        this.primaryOperand = result
+
+        return result
+    }
+
+    chooseOperation(operation){
+        if(this.operation !== '') return
+        this.operation = operation
+        this.secondaryOperand = this.primaryOperand
+        this.primaryOperand = 0
     }
 
     clear(){
@@ -42,10 +100,14 @@ export default class Calculator {
 
 }
 
-const NUMBER_FORMATTER =  new Intl.NumberFormat("en", {
-    maximumFractionDigits: 20
-})
+const NUMBER_FORMATTER =  new Intl.NumberFormat("en")
 
 function displayNumber(number){
-    return NUMBER_FORMATTER.format(number)
+    const stringNumber = number?.toString() || ''
+    if (stringNumber === '') return ''
+    const [integer, decimal] = stringNumber.split('.')
+    const formattedInteger = NUMBER_FORMATTER.format(integer)
+    if (decimal == null) return formattedInteger
+    return formattedInteger + '.' + decimal
+
 }
